f3e17945c2d91aebb8110b791fd31bdb2279fe6c
DaoCongVinh <22130325@st.hcmuaf.edu.vn>
2024-11-07 20:51:19 +0700

    Merge remote-tracking branch 'origin/branchOfVinh' into branchOfVinh

---
+++ assets/scripts/product-details.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.related-products-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    });
+});
