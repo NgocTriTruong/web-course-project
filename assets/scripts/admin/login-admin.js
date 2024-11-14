0a30d59b8c14c76dd33ee18928f7a42bb1146013
DaoCongVinh <22130325@st.hcmuaf.edu.vn>
2024-11-14 16:04:41 +0700

    Merge remote-tracking branch 'origin/branchOfVinh' into branchOfVinh

---
+++ assets/scripts/admin/login-admin.js
@@ -0,0 +1,13 @@
+document.getElementById('login-form').addEventListener('submit', function (event){
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    //Kiem tra username va pass co dung hay khong?
+    if (username === 'admin' && password === 'admin') {
+        window.location.replace('admin_home.html');
+    }else{
+        alert("Tai khoan hoac mat khau da sai. Vui long kiem tra lai !")
+    }
+})
