dcf65bcd1ad9648825f2962f9d5e5bef2c90e9b8
TruongNgocTri12345 <truongngoctri12345@gmail.com>
2024-12-23 18:34:57 +0700

    dbconnect and productDao

--- AnimalFeed_webApp/src/main/java/vn/edu/hcmuaf/fit/animalfeed_webapp/dao/ProductDao.java
+++ AnimalFeed_webApp/src/main/java/vn/edu/hcmuaf/fit/animalfeed_webapp/dao/ProductDao.java
@@ -1,17 +1,61 @@
 package vn.edu.hcmuaf.fit.animalfeed_webapp.dao;
 
+import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.db.DBConnect;
 import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.model.Product;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 public class ProductDao {
 
     public List<Product> getAll() {
-        return new ArrayList<>();
+        Statement statement = DBConnect.get();
+        ResultSet re = null;
+        ArrayList<Product> listProduct = new ArrayList<>();
+
+        try {
+            re =  statement.executeQuery("SELECT * FROM products");
+            while (re.next()) {
+                System.out.println(re.getInt("id")
+                        + "; " + re.getInt("cat_Id")
+                        + "; " + re.getString("name")
+                        + "; " + re.getDouble("price")
+                        + "; " + re.getString("description")
+                        + "; " + re.getInt("quantity")
+                        + ";"  + re.getInt("status")
+                        + "; " + re.getString("img")
+                        + "; " + re.getDate("create_date"));
+            }
+            return listProduct;
+
+        } catch (SQLException e) {
+            return listProduct;
+        }
     }
 
     public Product getById(int id) {
-        return null;
+        Statement statement = DBConnect.get();
+        ResultSet re = null;
+        try {
+            re =  statement.executeQuery("SELECT * FROM products WHERE id = " + id);
+            if (re.next()) {
+                System.out.println(re.getInt("id")
+                        + "; " + re.getInt("cat_Id")
+                        + "; " + re.getString("name")
+                        + "; " + re.getDouble("price")
+                        + "; " + re.getString("description")
+                        + "; " + re.getInt("quantity")
+                        + ";"  + re.getInt("status")
+                        + "; " + re.getString("img")
+                        + "; " + re.getDate("create_date"));
+            }
+            return null;
+
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
 }
