f7a558d26b05ee0d7e7f6a1334474497ce05740a
TuanKhoaNLU <22130123@st.hcmuaf.edu.vn>
2024-12-23 20:33:45 +0700

    OrderDAO & UserDAO

--- AnimalFeed_webApp/src/main/java/vn/edu/hcmuaf/fit/animalfeed_webapp/dao/UserDao.java
+++ AnimalFeed_webApp/src/main/java/vn/edu/hcmuaf/fit/animalfeed_webapp/dao/UserDao.java
@@ -1,115 +1,57 @@
-//package vn.edu.hcmuaf.fit.animalfeed_webapp.dao;
-//
-//import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.db.DBConnect;
-//import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.model.User;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.*;
-//
-//public class UserDao {
-//    // Method to fetch a user by their username
-//    public ArrayList<User> getUserByName(String full_name) throws SQLException, ClassNotFoundException {
-//        Statement statement = DBConnect.get();
-//        ResultSet re = null;
-//        ArrayList<User> listUser = new ArrayList<>();
-//
-//        try {
-//            re =  statement.executeQuery("SELECT * FROM users WHERE full_name = '" + full_name + "'");
-//            while (re.next()) {
-//                System.out.println(re.getInt("id")
-//                        + "; " + re.getString("full_name")
-//                        + "; " + re.getString("password")
-//                        + "; " + re.getString("phone")
-//                        + "; " + re.getInt("status")
-//                        + "; " + re.getDate("create_date")
-//                        + ";"  + re.getDate("update_date")
-//                        + "; " + re.getInt("role"));
-//            }
-//            return listUser;
-//
-//        } catch (SQLException e) {
-//            return listUser;
-//        }
-//    }
-//
-//    // Method to insert a new user
-////    public ArrayList<User> getUserById(int id) throws SQLException, ClassNotFoundException {
-////        Statement statement = DBConnect.get();
-////        ResultSet re = null;
-////        ArrayList<User> listUser = new ArrayList<>();
-////
-////        try {
-////            re =  statement.executeQuery("SELECT * FROM users WHERE full_name = '" + full_name + "'");
-////            while (re.next()) {
-////                System.out.println(re.getInt("id")
-////                        + "; " + re.getInt("cat_Id")
-////                        + "; " + re.getString("name")
-////                        + "; " + re.getDouble("price")
-////                        + "; " + re.getString("description")
-////                        + "; " + re.getInt("quantity")
-////                        + ";"  + re.getInt("status")
-////                        + "; " + re.getString("img")
-////                        + "; " + re.getDate("create_date"));
-////            }
-////            return listUser;
-////
-////        } catch (SQLException e) {
-////            return listUser;
-////        }
-////    }
-////
-////    // Method to update user information
-////    public boolean updateUser(User user) {
-////        String sql = "UPDATE users SET password = ?, email = ?, role = ? WHERE username = ?";
-////        try (Connection connection = DatabaseConnection.getConnection();
-////             PreparedStatement statement = connection.prepareStatement(sql)) {
-////            statement.setString(1, user.getPassword());
-////            statement.setString(2, user.getEmail());
-////            statement.setString(3, user.getRole());
-////            statement.setString(4, user.getUsername());
-////            return statement.executeUpdate() > 0;
-////        } catch (SQLException e) {
-////            e.printStackTrace(); // Log the exception
-////        }
-////        return false;
-////    }
-////
-////    // Method to delete a user by username
-////    public boolean deleteUser(String username) {
-////        String sql = "DELETE FROM users WHERE username = ?";
-////        try (Connection connection = DatabaseConnection.getConnection();
-////             PreparedStatement statement = connection.prepareStatement(sql)) {
-////            statement.setString(1, username);
-////            return statement.executeUpdate() > 0;
-////        } catch (SQLException e) {
-////            e.printStackTrace(); // Log the exception
-////        }
-////        return false;
-////    }
-////
-////    // Method to list all users
-////    public List<User> getAllUsers() {
-////        List<User> userList = new ArrayList<>();
-////        String sql = "SELECT * FROM users";
-////        try (Connection connection = DatabaseConnection.getConnection();
-////             PreparedStatement statement = connection.prepareStatement(sql)) {
-////            ResultSet resultSet = statement.executeQuery();
-////            while (resultSet.next()) {
-////                User user = new User(
-////                        resultSet.getInt("id"),
-////                        resultSet.getString("username"),
-////                        resultSet.getString("password"),
-////                        resultSet.getString("email"),
-////                        resultSet.getString("role")
-////                );
-////                userList.add(user);
-////            }
-////        } catch (SQLException e) {
-////            e.printStackTrace(); // Log the exception
-////        }
-////        return userList;
-////    }
-////}
-//}
+package vn.edu.hcmuaf.fit.animalfeed_webapp.dao;
+
+import org.jdbi.v3.core.Jdbi;
+import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.db.JdbiConnect;
+import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.model.User;
+
+import java.sql.SQLException;
+import java.util.*;
+
+public class UserDao {
+    // Method to fetch a user by their username
+    public ArrayList<User> getUserByName(String name) throws SQLException, ClassNotFoundException {
+        Jdbi jdbi = JdbiConnect.getJdbi();
+        return (ArrayList<User>) jdbi.withHandle(handle -> handle.createQuery("select * from users like :namePattern").bind("pattern", "%" + name).mapToBean(User.class).list());
+    }
+
+    // Method to insert a new user
+    public User getUserById(int id) throws SQLException, ClassNotFoundException {
+        Jdbi jdbi = JdbiConnect.getJdbi();
+        return jdbi.withHandle(handle -> handle.createQuery("select * from users where id = :id").bind("id", id).mapToBean(User.class).findOne().orElse(null));
+    }
+
+    // Method to update user information
+    public void updateUser(User user) {
+        Jdbi jdbi = JdbiConnect.getJdbi();
+        int rowsUpdated = jdbi.withHandle(handle ->
+                handle.createUpdate("UPDATE users SET fullName = :fullName, password = :password, phone = :phone, status = :status, createDate = :createDate, updateDate = :updateDate, role = :role WHERE id = :id")
+                        .bind("fullName", user.getFullName())
+                        .bind("password", user.getPassword())
+                        .bind("phone", user.getPhone())
+                        .bind("status", user.getStatus())
+                        .bind("createDate", user.getCreateDate())
+                        .bind("updateDate", user.getUpdateDate())
+                        .bind("role", user.getRole())
+                        .bind("id", user.getId())
+                        .execute()
+        );
+        System.out.println(rowsUpdated);
+    }
+
+    // Method to delete a user by username
+    public void deleteUser(int id) {
+        Jdbi jdbi = JdbiConnect.getJdbi();
+        int rowsDeleted = jdbi.withHandle(handle ->
+                handle.createUpdate("DELETE FROM users WHERE id = :id")
+                        .bind("id", id)
+                        .execute()
+        );
+        System.out.println(rowsDeleted);
+    }
+
+    // Method to list all users
+    public ArrayList<User> getAllUsers() {
+        Jdbi jdbi = JdbiConnect.getJdbi();
+        return (ArrayList<User>) jdbi.withHandle(handle -> handle.createQuery("select * from users").mapToBean(User.class).list());
+    }
+}
