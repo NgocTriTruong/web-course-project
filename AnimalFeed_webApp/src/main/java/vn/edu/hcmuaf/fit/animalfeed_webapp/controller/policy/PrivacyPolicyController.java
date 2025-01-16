930ca24c2d2f135be5f3c741f8d8cdcaa4cecf1e
TruongNgocTri12345 <truongngoctri12345@gmail.com>
2025-01-16 11:34:23 +0700

    Thư viện , tư vấn thuật kĩ thuat và các chính sách

--- AnimalFeed_webApp/src/main/java/vn/edu/hcmuaf/fit/animalfeed_webapp/controller/policy/PrivacyPolicyController.java
+++ AnimalFeed_webApp/src/main/java/vn/edu/hcmuaf/fit/animalfeed_webapp/controller/policy/PrivacyPolicyController.java
@@ -4,14 +4,22 @@ package vn.edu.hcmuaf.fit.animalfeed_webapp.controller.policy;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.model.Category;
+import vn.edu.hcmuaf.fit.animalfeed_webapp.services.CategoryService;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "PrivacyPolicyController", value = "/privacy-policy")
 public class PrivacyPolicyController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws SecurityException, IOException, ServletException {
+        CategoryService categoryService = new CategoryService();
+
+        List<Category> categories = categoryService.getAll();
+        request.setAttribute("categoriesData", categories);
+
         request.getRequestDispatcher("views/web/privacy_policy.jsp").forward(request, response);
     }
 
