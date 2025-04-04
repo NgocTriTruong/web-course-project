2fe8d694a22b6996be7b777c1069b8d9ec15cca2
TruongNgocTri12345 <truongngoctri12345@gmail.com>
2025-04-05 00:35:23 +0700

    Triển khai Ghi nhận Hành động cho các thao tác của admin

---
+++ AnimalFeed_webApp/src/main/java/vn/edu/hcmuaf/fit/animalfeed_webapp/dao/ActionLogDao.java
@@ -0,0 +1,21 @@
+package vn.edu.hcmuaf.fit.animalfeed_webapp.dao;
+
+import org.jdbi.v3.core.Jdbi;
+import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.db.JdbiConnect;
+import vn.edu.hcmuaf.fit.animalfeed_webapp.dao.model.ActionLog;
+
+public class ActionLogDao {
+    private static Jdbi jdbi = JdbiConnect.getJdbi();
+
+    // Method to insert a new action log
+    public static void logAction(ActionLog actionLog) {
+        jdbi.useHandle(handle -> {
+            handle.createUpdate("INSERT INTO action_log (user_id, action_type, entity_type, " +
+                            "entity_id, created_at, description, before_data, after_data) " +
+                            "VALUES (:user_id, :action_type, :entity_type, :entity_id, " +
+                            ":created_at, :description, :before_data, :after_data)")
+                    .bindBean(actionLog)
+                    .execute();
+        });
+    }
+}
